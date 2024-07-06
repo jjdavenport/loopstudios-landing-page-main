468c41d69506c58ee2de86011743376c206a4b74
jjdavenport <jordanjamesdavenport@gmail.com>
2024-07-06 14:03:03 +0100

    added mobile styling

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("test");
+const navBtn = document.getElementById("navBtn");
+const navDialogCloseBtn = document.getElementById("navDialogCloseBtn");
+
+navBtn.addEventListener("click", () => {
+  const navDialog = document.getElementById("navDialog");
+  navDialog.classList.toggle("nav__dialog--active");
+  document.body.style.overflow = "hidden";
+});
+
+navDialogCloseBtn.addEventListener("click", () => {
+  const navDialog = document.getElementById("navDialog");
+  navDialog.classList.remove("nav__dialog--active");
+  document.body.style.overflow = "auto";
+});
